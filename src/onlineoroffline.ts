6ee52ff16ecc93d5965c76c36c97b252bfe16dea
Kazuhiro-Tokumoto <20250148@st.shudo-h.ed.jp>
2025-12-21 21:26:55 +0900

    friend

---
+++ src/onlineoroffline.ts
@@ -0,0 +1,26 @@
+
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+
+// 初期化（ここはマインさんの既存コード通り）
+const myUuid = localStorage.getItem("my_uuid"); 
+const supabaseUrl = 'https://cedpfdoanarzyxcroymc.supabase.co'
+const supabaseKey = 'sb_publishable_E5jwgv5t2ONFKg3yFENQmw_lVUSFn4i'
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
+export async function checkUserOnline(targetUuid: string) {
+    const { data, error } = await supabase
+        .from('profile_users')
+        .select('is_active, name')
+        .eq('uuid', targetUuid)
+        .single(); // 1件だけ取得
+
+    if (error) {
+        console.error("状態取得エラー:", error.message);
+        return false;
+    }
+
+    if (data) {
+        console.log(`${data.name} さんの状態: ${data.is_active ? "オンライン" : "オフライン"}`);
+        return data.is_active;
+    }
+    return false;
+}
